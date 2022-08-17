6a1e0670f4bd9cfc0a63b3342415a2b04f409eda
Nasser Alhseen <abdalansralhseen2019@gmail.com>
2022-08-17 14:05:31 +0300

    resume update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,7 @@ import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.jpeg";
 import editor from "../../Assets/Projects/codeEditor.png";
 import boss from "../../Assets/boss.png";
+import zmc from"../../Assets/zmc.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import pafion from "../../Assets/pafion.png";
 
@@ -34,6 +35,15 @@ function Projects() {
 
           <Col  md={4} className="project-card">
             <ProjectCard 
+              imgPath={zmc}
+              isBlog={false}
+              title="ZMC Domestic Cargo"
+              description="This App for suppliers registered by ZMC company and deals with them to for following their orders and keep tracking status changes on at home delivery orders inside Iraq."
+              link="https://play.google.com/store/apps/details?id=com.zmc.dcmgmt&hl=en&gl=US"
+            />
+          </Col>
+          <Col  md={4} className="project-card">
+            <ProjectCard 
               imgPath={pafion}
               isBlog={false}
               title="Pafion Queen"
@@ -42,46 +52,7 @@ function Projects() {
             />
           </Col>
 
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
-            />
-          </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              link="https://github.com/soumyajit4419/Plant_AI"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
